4854130f7fb11d3b4aa1cdf54cad0eb8bb86ed39
vaziliybober <vaziliybober@gmail.com>
2021-04-28 17:14:01 +0300

    add insert, remove

--- __tests__/some.test.js
+++ __tests__/some.test.js
@@ -1,5 +1,16 @@
 import sum from '../src/index.js';
+import { insert, remove } from '../src/index.js';
 
 test('some test', () => {
   expect(sum(2, 2)).toBe(4);
+});
+
+test('insert', () => {
+  expect(insert('hello world!', 5, ',')).toBe('hello, world!');
+});
+
+test('remove', () => {
+  expect(remove('hello, world!', 5)).toBe('hello world!');
+  expect(remove('hello, world!', 0)).toBe('ello, world!');
+  expect(remove('hello, world!', 12)).toBe('hello, world');
 });
--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 export default (a, b) => a + b;
+
+export const insert = (text, i, symbol) => {
+  return text.substr(0, i) + symbol + text.substr(i);
+};
+
+export const remove = (text, i) => {
+  return text.substr(0, i) + text.substr(i + 1);
+};
